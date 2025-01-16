5d5a8ab66c8ccc8e5b0d61a5e467a77e4c2c9677
esteban1318 <poveda1318@gmail.com>
2025-01-16 04:56:20 -0500

    se modifica archivo ejercicio_2.java

--- src/main/java/Interfaces/interfaz_codigo.java
+++ src/main/java/Interfaces/interfaz_codigo.java
@@ -9,15 +9,17 @@ public class interfaz_codigo {
 
         // Crear la ventana
         JFrame frame = new JFrame("Mi Ventana");
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         frame.setSize(500, 600 ); // Tamaño de la ventana
 
         // Crear la instancia de ejercicio_2
-        ejercicio_2 operacion = new ejercicio_2();
+        ejercicio_2 operacion=new ejercicio_2(frame);
 
         // Agregar el panel al JFrame
         frame.add(operacion.getOp());
-        
+       
         
         
 
